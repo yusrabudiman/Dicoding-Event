8c27d505ff3f139b74d58a81931f448105b13137
yusrabudiman <enkidu.risk@gmail.com>
2024-10-18 13:47:19 +0700

    .

--- app/src/main/java/com/example/dicodingevent/SplashScreen.kt
+++ app/src/main/java/com/example/dicodingevent/SplashScreen.kt
@@ -26,10 +26,5 @@ class SplashScreen : AppCompatActivity() {
             startActivity(intent)
             finish()
         }
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
-        }
     }
 }
