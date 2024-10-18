021ada0fa0b5043c6ca83dfff7b3f6ba21ceb9a7
yusrabudiman <enkidu.risk@gmail.com>
2024-10-18 12:42:40 +0700

    .

--- app/src/main/java/com/example/dicodingevent/ui/detail/DetailEvent.kt
+++ app/src/main/java/com/example/dicodingevent/ui/detail/DetailEvent.kt
@@ -7,7 +7,6 @@ import android.os.Bundle
 import android.text.method.LinkMovementMethod
 import android.view.MenuItem
 import android.view.View
-import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.text.HtmlCompat
